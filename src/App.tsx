f042c8549a90e7e2d03f1e1ef8a6ea4613c0e497
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-10-11 14:18:15 -0400

    Resolved type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import Map from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import baseMapStyles from "./data/basemap.json";
-import CONFIG from "./config"
-
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import CONFIG from "./config";
 
 function App() {
   return (
@@ -10,13 +8,16 @@ function App() {
       initialViewState={{
         longitude: CONFIG.INITIALVIEW.LONG,
         latitude: CONFIG.INITIALVIEW.LAT,
-        zoom: CONFIG.INITIALVIEW.ZOOM
+        zoom: CONFIG.INITIALVIEW.ZOOM,
       }}
       maxZoom={CONFIG.BOUNDS.ZOOM.MAX}
       minZoom={CONFIG.BOUNDS.ZOOM.MIN}
-      maxBounds={[[CONFIG.BOUNDS.WEST, CONFIG.BOUNDS.SOUTH], [CONFIG.BOUNDS.EAST, CONFIG.BOUNDS.NORTH]]}
-      style={{width: CONFIG.STYLE.WIDTH, height: CONFIG.STYLE.HEIGHT}}
-      mapStyle={baseMapStyles}
+      maxBounds={[
+        [CONFIG.BOUNDS.WEST, CONFIG.BOUNDS.SOUTH],
+        [CONFIG.BOUNDS.EAST, CONFIG.BOUNDS.NORTH],
+      ]}
+      style={{ width: CONFIG.STYLE.WIDTH, height: CONFIG.STYLE.HEIGHT }}
+      mapStyle={CONFIG.MAPSTYLE}
     />
   );
 }
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export default ({
+export default {
   INITIALVIEW: {
     LONG: -74.0008,
     LAT: 40.7018,
@@ -8,15 +8,16 @@ export default ({
     WEST: -74.3308,
     SOUTH: 40.2989,
     EAST: -73.6311,
-    NORTH: 41.1030,
+    NORTH: 41.103,
     ZOOM: {
       MIN: 9.5,
       MAX: 20,
-    }
+    },
   },
   STYLE: {
     WIDTH: window.innerWidth,
     HEIGHT: window.innerHeight,
   },
-
-});
+  MAPSTYLE:
+    "https://raw.githubusercontent.com/NYCPlanning/equity-tool/main/src/data/basemap.json",
+};
