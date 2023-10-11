1ab62a44ab3dbf2d9dbc67197414e096a1ca4292
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-10-11 11:05:03 -0400

    Map displaying

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export default ({
+  INITIALVIEW: {
+    LONG: -74.0008,
+    LAT: 40.7018,
+    ZOOM: 11,
+  },
+  BOUNDS: {
+    WEST: -74.3308,
+    SOUTH: 40.2989,
+    EAST: -73.6311,
+    NORTH: 41.1030,
+    ZOOM: {
+      MIN: 9.5,
+      MAX: 20,
+    }
+  },
+  STYLE: {
+    WIDTH: window.innerWidth,
+    HEIGHT: window.innerHeight,
+  },
+
+});
