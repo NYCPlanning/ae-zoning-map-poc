ef7e2e7c084ec270f0040013c878ed6015638b93
David Hochbaum <dhochbaum@planning.nyc.gov>
2023-11-24 16:47:42 -0500

    Refactored to remove unneccessary component

--- src/components/ZoningDistrictFilters.tsx
+++ src/components/ZoningDistrictFilters.tsx
@@ -1,20 +1,57 @@
-import { Accordion } from "@nycplanning/streetscape";
-import { useGetZoningDistrictClassCategoryColors } from "../gen";
-import ZoningDistrictClassCategory from "./ZoningDistrictClassCategory";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Checkbox,
+  Spacer,
+  Stack,
+  Switch,
+} from "@nycplanning/streetscape";
+import LegendSquare from "./LegendSquare";
+import {
+  useGetZoningDistrictClassCategoryColors,
+  useGetZoningDistrictClasses,
+} from "../gen";
 
 function ZoningDistrictFilters() {
   const { data: classCategories } = useGetZoningDistrictClassCategoryColors();
+  const { data: classes } = useGetZoningDistrictClasses();
 
   return (
     <Accordion allowMultiple allowToggle>
       {classCategories
         ?.sort((a, b) => a.category.localeCompare(b.category))
         .map((category) => (
-          <ZoningDistrictClassCategory
-            key={category.category}
-            category={category.category}
-            color={category.color}
-          />
+          <AccordionItem key={category.category}>
+            <AccordionButton px={0} _hover={{ border: 0 }}>
+              <Switch size="sm" pr={2} />
+              {category.category} Districts
+              <LegendSquare color={category.color} />
+              <Spacer />
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>
+              <Stack
+                direction={["column", "row"]}
+                spacing={8}
+                flexWrap={"wrap"}
+                justifyContent={"flex-start"}
+                px={9}
+              >
+                {classes
+                  ?.filter((x) => {
+                    return x.category == category.category;
+                  })
+                  .map((c) => (
+                    <Checkbox size="sm" key={c.id}>
+                      {c.id}
+                    </Checkbox>
+                  ))}
+              </Stack>
+            </AccordionPanel>
+          </AccordionItem>
         ))}
     </Accordion>
   );
