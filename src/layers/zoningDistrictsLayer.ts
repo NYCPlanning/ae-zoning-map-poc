afc922c0384376649edff0f1bd9b645b80f34c40
David Hochbaum <dhochbaum@planning.nyc.gov>
2023-11-27 16:53:06 -0500

    Pulling layer from api

--- src/layers/zoningDistrictsLayer.ts
+++ src/layers/zoningDistrictsLayer.ts
@@ -2,4 +2,10 @@ import { MVTLayer } from "@deck.gl/geo-layers/typed";
 
 export const zoningDistrictsLayer = new MVTLayer({
   id: "zoningDistricts",
+  data: `${
+    import.meta.env.VITE_ZONING_API_URL
+  }/zoning-districts/{z}/{x}/{y}.pbf`,
+  getLineColor: [192, 0, 192],
+  getFillColor: [140, 170, 180],
+  visible: true,
 });
