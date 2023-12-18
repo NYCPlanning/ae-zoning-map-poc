b586091d4e12a6e9b5e52c496edde8da5ab54385
horatio <horatiorosa@aol.com>
2023-12-18 14:21:13 -0500

    regenerate Kubb to include zoing district classes

--- src/gen/types/GetZoningDistrictClassCategoryColors.ts
+++ src/gen/types/GetZoningDistrictClassCategoryColors.ts
@@ -1,4 +1,9 @@
+import type { Error } from "./Error";
 import type { ZoningDistrictClassCategoryColor } from "./ZoningDistrictClassCategoryColor";
+
+export type GetZoningDistrictClassCategoryColors400 = Error;
+
+export type GetZoningDistrictClassCategoryColors500 = Error;
 
 /**
  * @description An object containing all zoning district category colors.
